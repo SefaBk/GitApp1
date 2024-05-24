7adae00eefaaa090a07191d096ebdd76c9e9d637
Sefa <sefabkucuk@gmail.com>
2024-05-24 21:24:19 +0200

    v1

---
+++ src/main/java/Runner.java
@@ -0,0 +1,17 @@
+public class Runner {
+    public static void main(String[] args) {
+
+        //version 1
+        System.out.println("hello GIT");
+        System.out.println("git add . : staging areaya alir.");
+
+
+
+
+
+
+
+
+
+    }
+}
